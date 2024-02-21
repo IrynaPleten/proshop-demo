78eb4e85c15ec608f8075adf757e8212333f7e81
Плетень Ірина <pleten.iryna@gmail.com>
2024-02-21 19:55:42 +0200

    Qty & Add To Cart Handler

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,42 +1,51 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice } from '@reduxjs/toolkit'
 
-const initialState = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : {cartItems: []}
+const initialState = localStorage.getItem('cart')
+  ? JSON.parse(localStorage.getItem('cart'))
+  : { cartItems: [] }
 
 const addDecimals = num => {
-	return (Math.round(num * 100) / 100).toFixed()
+  return (Math.round(num * 100) / 100).toFixed()
 }
 
 const cartSlice = createSlice({
-	name: 'cart',
-	initialState,
-	reducers: {
-		addToCart: (state, action) => {
-			const item = action.payload
+  name: 'cart',
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      const item = action.payload
 
-			const existItem = state.cartItem.find(x => x._id === item._id)
+      const existItem = state.cartItems.find(x => x._id === item._id)
 
-			if(existItem){
-				state.cartItems = state.cartItems.map(x => x._id === existItem._id ? item : x)
-			} else {
-				state.cartItems = [...state.cartItems, item]
-			}
+      if (existItem) {
+        state.cartItems = state.cartItems.map(x =>
+          x._id === existItem._id ? item : x
+        )
+      } else {
+        state.cartItems = [...state.cartItems, item]
+      }
 
-			//calculate items price
-			state.itemsPrice = addDecimals(state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0))
-			//calculate shipping price
-			state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10)
-			//calculate tax price
-			state.taxPrice = addDecimals(Number((0.15 * state.itemsPrice).toFixed(2)))
-			//calculate total price
-			state.totalPrice = (
-				Number(state.itemsPrice) +
-				Number(state.shippingPrice) +
-				Number(state.taxPrice)
-			).toFixed(2)
+      // Calculate items price
+      state.itemsPrice = addDecimals(
+        state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+      )
 
-			localStorage.setItem('cart', JSON.stringify(state))
-		},
-	},
+      // Calculate shipping price
+      state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10)
+
+      // Calculate tax price
+      state.taxPrice = addDecimals(Number((0.15 * state.itemsPrice).toFixed(2)))
+
+      // Calculate total price
+      state.totalPrice = (
+        Number(state.itemsPrice) +
+        Number(state.shippingPrice) +
+        Number(state.taxPrice)
+      ).toFixed(2)
+
+      localStorage.setItem('cart', JSON.stringify(state))
+    },
+  },
 })
 
 export const { addToCart } = cartSlice.actions
