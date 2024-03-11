0109ed84ac9d46230883f0a990e5168b6e488ac5
Плетень Ірина <pleten.iryna@gmail.com>
2024-03-11 18:56:43 +0100

    Auth Middleware & Endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,93 +2,93 @@ import asyncHandler from '../middleware/asyncHandler.js'
 import User from '../models/userModel.js'
 import jwt from 'jsonwebtoken'
 
-// @desc    Auth user & get token
-// @route   POST /api/users/login
+// @desc		Auth use & get token
+// @route		POST /api/users/login
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
-  const { email, password } = req.body
+	const { email, password } = req.body
 
-  const user = await User.findOne({ email })
+	const user = await User.findOne({ email })
 
-  if (user && (await user.matchPassword(password))) {
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-      expiresIn: '30d',
-    })
+	if (user && (await user.matchPassword(password))) {
+		const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+			expiresIn: '30d',
+		})
 
-    // Set JWT as HTTP-only cookie
-    res.cookie('jwt', token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV !== 'development',
-      sameSite: 'strict',
-      maxAge: 30 * 24 * 60 * 60 * 1000, // 30 Days
-    })
+		// Set JWT as HTTP-only cookie
+		res.cookie('jwt', token, {
+			httpOnly: true,
+			secure: process.env.NODE_ENV !== 'development',
+			sameSite: 'strict',
+			maxAge: 30 * 24 * 60 * 60 * 1000, // 30 Days
+		})
 
-    res.json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      isAdmin: user.isAdmin,
-    })
-  } else {
-    res.status(401)
-    throw new Error('Invalid email or password')
-  }
+		res.json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			isAdmin: user.isAdmin,
+		})
+	} else {
+		res.status(401)
+		throw new Error('Invalid email or password')
+	}
 })
 
-// @desc    Register user
-// @route   POST /api/users
+// @desc		Register user
+// @route		POST /api/users
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
-  res.send('register user')
+	res.send('register user')
 })
 
-// @desc    Logout user / clear cookies
-// @route   POST /api/users/logout
+// @desc		Logout user / clear cookie
+// @route		POST /api/users/logout
 // @access  Private
 const logoutUser = asyncHandler(async (req, res) => {
-  res.send('logout user')
+	res.send('logout user')
 })
 
-// @desc    Get user profile
-// @route   GET /api/users/profile
+// @desc		Get user profile
+// @route		GET /api/users/profile
 // @access  Public
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.send('get user profile')
+	res.send('get user profile')
 })
 
-// @desc    Update user profile
-// @route   POST /api/users/logout
+// @desc		Update user profile
+// @route		PUT /api/users/profile
 // @access  Public
 const updateUserProfile = asyncHandler(async (req, res) => {
-  res.send('update user profile')
+	res.send('update user profile')
 })
 
-// @desc    Get users
-// @route   GET /api/users
+// @desc		Get users
+// @route		Get /api/users
 // @access  Private/Admin
 const getUsers = asyncHandler(async (req, res) => {
-  res.send('get users')
+	res.send('get users')
 })
 
-// @desc    Get user by ID
-// @route   GET /api/users/:id
+// @desc		Get user by ID
+// @route		Get /api/users/:id
 // @access  Private/Admin
-const getUserById = asyncHandler(async (req, res) => {
-  res.send('get user by id')
+const getUserByID = asyncHandler(async (req, res) => {
+	res.send('get user by id')
 })
 
-// @desc    Delete user
-// @route   DELETE /api/users/:id
+// @desc		Delete user
+// @route		DELETE /api/users/:id
 // @access  Private/Admin
 const deleteUser = asyncHandler(async (req, res) => {
-  res.send('delete user')
+	res.send('delete user')
 })
 
-// @desc    Update user
-// @route   PUT /api/users/:id
+// @desc		Update user
+// @route		PUT /api/users/:id
 // @access  Private/Admin
 const updateUser = asyncHandler(async (req, res) => {
-  res.send('update user')
+	res.send('update user')
 })
 
 export {
@@ -99,6 +99,6 @@ export {
 	updateUser,
 	updateUserProfile,
 	getUsers,
-	getUserById,
+	getUserByID,
 	deleteUser,
 }
