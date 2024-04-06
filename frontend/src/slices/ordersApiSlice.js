5154541cd88e9d5870f2a7e5605c77bdee873c69
Плетень Ірина <pleten.iryna@gmail.com>
2024-04-06 23:40:18 +0200

    Order Page

--- frontend/src/slices/ordersApiSlice.js
+++ frontend/src/slices/ordersApiSlice.js
@@ -1,4 +1,4 @@
-import {apiSlice} from './apiSlice'
+import { apiSlice } from './apiSlice'
 import { ORDERS_URL } from '../constants'
 
 export const orderApiSlice = apiSlice.injectEndpoints({
@@ -6,11 +6,17 @@ export const orderApiSlice = apiSlice.injectEndpoints({
 		createOrder: builder.mutation({
 			query: order => ({
 				url: ORDERS_URL,
-				method:'POST',
-				body: { ...order},
-			})
-		})
-	})
+				method: 'POST',
+				body: { ...order },
+			}),
+		}),
+		getOrderDetails: builder.query({
+			query: orderId => ({
+				url: `${ORDERS_URL}/${orderId}`,
+			}),
+			keepUnusedDataFor: 5,
+		}),
+	}),
 })
 
-export const {useCreateOrderMutation} = orderApiSlice
+export const { useCreateOrderMutation, useGetOrderDetailsQuery } = orderApiSlice
