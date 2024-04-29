a92e26475dbb99b74fa5e0387f4508d852d36320
Плетень Ірина <pleten.iryna@gmail.com>
2024-04-29 17:32:44 +0200

    Paginate Products

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,9 +1,11 @@
 import { Col, Row } from 'react-bootstrap'
 import { Product, Loader, Message } from '@components'
 import { useGetProductsQuery } from '@slices/productsApiSlice'
+import {useParams} from 'react-router-dom'
 
 const HomeScreen = () => {
-	const { data: products, isLoading, error } = useGetProductsQuery()
+	const {pageNumber} = useParams()
+	const { data, isLoading, error } = useGetProductsQuery({ pageNumber })
 
 	return (
 		<>
@@ -17,7 +19,7 @@ const HomeScreen = () => {
 				<>
 					<h1>Latest Products</h1>
 					<Row>
-						{products.map(product => (
+						{data.products.map(product => (
 							<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
 								<Product product={product} />
 							</Col>
