38ac22a482ac2e6ce150ef0dd4f2e23f02d8101c
Плетень Ірина <pleten.iryna@gmail.com>
2024-08-22 13:46:14 +0300

    Final project

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -1,19 +1,24 @@
-import { Link } from 'react-router-dom';
-import { Carousel, Image } from 'react-bootstrap';
-import Message from './Message';
-import { useGetTopProductsQuery } from '../slices/productsApiSlice';
+import { Link } from 'react-router-dom'
+import { Carousel, Image } from 'react-bootstrap'
+import Message from './Message'
+import { useGetTopProductsQuery } from '../slices/productsApiSlice'
 
 const ProductCarousel = () => {
-  const { data: products, isLoading, error } = useGetTopProductsQuery();
+  const { data: products, isLoading, error } = useGetTopProductsQuery()
 
   return isLoading ? null : error ? (
     <Message variant='danger'>{error?.data?.message || error.error}</Message>
   ) : (
     <Carousel pause='hover' className='bg-primary mb-4'>
-      {products.map((product) => (
+      {products.map(product => (
         <Carousel.Item key={product._id}>
           <Link to={`/product/${product._id}`}>
-            <Image src={product.image} alt={product.name} fluid />
+            <Image
+              src={product.image}
+              alt={product.name}
+              fluid
+              className='d-block mx-auto'
+            />
             <Carousel.Caption className='carousel-caption'>
               <h2 className='text-white text-right'>
                 {product.name} (${product.price})
@@ -23,7 +28,8 @@ const ProductCarousel = () => {
         </Carousel.Item>
       ))}
     </Carousel>
-  );
-};
+  )
+}
 
-export default ProductCarousel;
+export default ProductCarousel
+
